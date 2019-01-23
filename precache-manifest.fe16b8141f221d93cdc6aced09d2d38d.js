7f6ee88e5aae8ed634f86579e2db3970d3ef763b
Adenilson Araújo dos Santos <adenilsonblog@gmail.com>
2019-01-23 15:20:58 -0300

    Updates

---
+++ precache-manifest.fe16b8141f221d93cdc6aced09d2d38d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "36e3e00e663ebd311522",
+    "url": "/myrepos/static/js/main.36e3e00e.chunk.js"
+  },
+  {
+    "revision": "11124abe128f0fcec16f",
+    "url": "/myrepos/static/css/1.bb194f8e.chunk.css"
+  },
+  {
+    "revision": "11124abe128f0fcec16f",
+    "url": "/myrepos/static/js/1.11124abe.chunk.js"
+  },
+  {
+    "revision": "80f87ebe59ee8df39afd",
+    "url": "/myrepos/static/js/runtime~main.80f87ebe.js"
+  },
+  {
+    "revision": "2a051dd6b6dc39cbfffcf4f3bc74e573",
+    "url": "/myrepos/static/media/coding.2a051dd6.svg"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/myrepos/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/myrepos/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/myrepos/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/myrepos/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/myrepos/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/myrepos/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/myrepos/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/myrepos/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/myrepos/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/myrepos/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/myrepos/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/myrepos/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/myrepos/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/myrepos/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/myrepos/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "4ecbca141dbfe3ac00a99535fda61f55",
+    "url": "/myrepos/index.html"
+  }
+];
